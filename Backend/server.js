1e64f67cda5dfa7ddcca590c90055fbdd0464c9a
CHEVULA SOMESH <146305649+Somesh2504@users.noreply.github.com>
2025-04-14 10:25:00 +0530

    Update server.js
    cors update

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const app=express();
 const PORT=process.env.PORT||5000
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: '*',
 };
 const __dirname = path.resolve()
 app.use(express.json())
