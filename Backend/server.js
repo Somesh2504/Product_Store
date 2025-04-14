fa7ffa173d2a6b5b0daa35e31a8e71c4459d6546
CHEVULA SOMESH <146305649+Somesh2504@users.noreply.github.com>
2025-04-14 13:09:40 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,13 +8,13 @@ const app=express();
 const PORT=process.env.PORT||5000
 
 const corsOptions = {
-    origin: '*',
+  origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
 };
 const __dirname = path.resolve()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors(corsOptions));
-
+dotenv.config();
 app.use('/api/products',productRoute)
 console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV==="production"){
